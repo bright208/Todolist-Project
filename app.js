f2503b44f5590b34ed22e9f975b6879464bc5229
Edison <nsysuilan@gmail.com>
2021-10-15 15:48:21 +0800

    add create todo function

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose') // 載入 mongoose
 const app = express()
 const exphbs = require("express-handlebars")
 const Todo = require('./models/todo') // 載入 Todo model
+const bodyParser = require('body-parser')
 
 mongoose.connect('mongodb://localhost/todo-list') // 設定連線到 mongoDB
 
@@ -23,12 +24,26 @@ app.set("view engine", "hbs")
 
 app.use(express.static("public"))
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // 設定首頁路由
 app.get('/', (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
+})
+
+app.get('/todos/new', (req, res) => {
+  res.render('new')
+})
+
+//新增資料庫的資料
+app.post('/todos', (req, res) => {
+  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+  return Todo.create({ name: name })     // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
 })
 
 // 設定 port 3000
